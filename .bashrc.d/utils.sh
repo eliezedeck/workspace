6cb861b88cf5c0a456e31787e6063564b6baa535
Elie Zedeck RANDRIAMIANDRIRAY <elie.zedeck@gmail.com>
2022-10-17 05:39:43 +0000

    Bash utils: topof

--- .bashrc.d/utils.sh
+++ .bashrc.d/utils.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function topof() {
   if [ -z "$1" ]; then
     echo "Opens-up top for the given process name, works even for multiple processes."
@@ -7,5 +9,5 @@ function topof() {
     echo
     return 1
   fi
-  top -p `pgrep -d ',' $1`
+  top $2 -p `pgrep -d ',' $1`
 }
